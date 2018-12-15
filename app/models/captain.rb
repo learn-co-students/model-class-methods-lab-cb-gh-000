eef1eb4353371d4674e4bbcacbc0762f72f25ca0
Aurangzaib Danial <aurangzaib.danial@gmail.com>
2018-12-15 18:44:21 +0500

    refractored captain unit tests solutions

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -9,46 +9,19 @@ class Captain < ActiveRecord::Base
   	by_boat_class("Sailboat")
   end
 
+  def self.motor_operators
+    by_boat_class("Motorboat")
+  end
+
   def self.talented_seafarers
-  	# Ruby way
-  	# all.map do |captain|
-  	# 	result = captain.classifications.where(name: ['Sailboat', 'Motorboat']).pluck(:name).uniq
-
-  	# 	if result.include?('Sailboat') && result.include?('Motorboat')
-  	# 		captain
-  	# 	end
-
-  	# end.compact
-  	
-  # 	query = "SELECT classifications.name, boats.id , classifications.id FROM boats JOIN boat_classifications ON boats.id = boat_classifications.boat_id
-		# JOIN classifications ON boat_classifications.classification_id = classifications.id
-		# WHERE captain_id = 2"
-
-#   	sql = <<-SQL
-# SELECT DISTINCT captains.*  FROM captains
-# INNER JOIN boats ON boats.captain_id = captains.id
-# iNNER JOIN boat_classifications ON boat_classifications.boat_id = boats.id
-# INNER JOIN classifications ON classifications.id = boat_classifications.classification_id
-# WHERE  classifications.name = 'Sailboat'  AND EXISTS (
-# SELECT DISTINCT(boats.captain_id) FROM boats JOIN boat_classifications ON boat_classifications.boat_id = boats.id
-# JOIN classifications ON boat_classifications.classification_id = classifications.id
-# WHERE boats.captain_id = captains.id AND classifications.name = 'Motorboat'
-# )
-#   	SQL
-
-    by_boat_class('Sailboat')
-    .where(Boat.joins(:classifications)
-      .where("boats.captain_id = captains.id AND classifications.name = 'Motorboat'")
-      .distinct.exists
-    )
-    
-  	end
+    where(id: sailors.pluck(:id) & motor_operators.pluck(:id))
+  end
 
   def self.by_boat_class(classification)
-  	joins(:classifications).where("classifications.name = ?", classification).distinct
+  	joins(:classifications).where(classifications: { name: classification }).distinct
   end
 
   def self.non_sailors
-    where.not(id: sailors)
+    where.not(id: sailors.pluck(:id))
   end
 end
